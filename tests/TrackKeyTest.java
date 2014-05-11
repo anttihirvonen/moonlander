065f41f5ef31bde1b997778c30f89f4e41410db7
Antti Hirvonen <antti.hirvonen@gmail.com>
2014-05-11 20:47:35 +0300

    Add Ant configurations for building and running tests
    Since testing in Processing is very time consuming, the build
    script now contains tasks to build and run JUnit tests.
    I included some example tests for testing the testing system (:D)

---
+++ tests/TrackKeyTest.java
@@ -0,0 +1,41 @@
+package rocketpower.tests;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import rocketpower.library.*;
+
+
+@RunWith(JUnit4.class)
+public class TrackKeyTest {
+    @Test
+    public void testCompareTo() {
+        TrackKey first = new TrackKey(1, 0.f, TrackKey.KeyType.STEP);
+        TrackKey second = new TrackKey(2, 0.f, TrackKey.KeyType.STEP);
+
+        assertEquals(first.compareTo(second), -1);
+        assertEquals(second.compareTo(first), 1);
+        assertEquals(second.compareTo(second), 0);
+    }
+
+    @Test
+    public void testInterpolate() {
+        TrackKey first = new TrackKey(1, 1.f, TrackKey.KeyType.STEP);
+        TrackKey second = new TrackKey(11, 2.f, TrackKey.KeyType.STEP);
+        assertEquals(TrackKey.interpolate(first, second, 1), 1.0, 0.0001);
+        assertEquals(TrackKey.interpolate(first, second, 10.9), 1.0, 0.0001);
+
+        first.setKeyType(TrackKey.KeyType.LINEAR);
+        assertEquals(TrackKey.interpolate(first, second, 1), 1.0, 0.0001);
+        assertEquals(TrackKey.interpolate(first, second, 6), 1.5, 0.0001);
+
+        first.setKeyType(TrackKey.KeyType.SMOOTH);
+        assertEquals(TrackKey.interpolate(first, second, 5), 1.352, 0.0001);
+
+        first.setKeyType(TrackKey.KeyType.RAMP);
+        assertEquals(TrackKey.interpolate(first, second, 5), 1.16, 0.0001);
+    }
+}
