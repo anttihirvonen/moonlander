216ba108743e5b89ac2b9c8e5ba957d9c5c24a5e
Antti Hirvonen <antti.hirvonen@gmail.com>
2014-05-11 12:03:00 +0300

    Implement interpolation between TrackKeys

--- src/rocketpower/library/TrackKey.java
+++ src/rocketpower/library/TrackKey.java
@@ -38,4 +38,22 @@ public class TrackKey implements Comparable<TrackKey> {
     public String toString() {
         return String.format("TrackKey(row=%d value=%f type=%s)", row, value, keyType);
     }
+
+    public static double interpolate(TrackKey first, TrackKey second, double row) {
+        double t = (row - first.row) / (second.row - first.row);
+
+        switch (first.keyType) {
+            case STEP:
+                return first.value;
+            //case KeyType.LINEAR:
+            case SMOOTH:
+                t = t * t * (3 - 2*t);
+                break;
+            case RAMP:
+                t = Math.pow(t, 2.0);
+                break;
+        }
+
+        return first.value + (second.value - first.value) * t;
+    }
 }
