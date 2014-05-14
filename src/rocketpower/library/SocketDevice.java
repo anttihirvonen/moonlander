938e9f1e61e9a2ee800e714e05ffed0b1da2cb57
Antti Hirvonen <antti.hirvonen@gmail.com>
2014-05-14 10:38:11 +0300

    SocketDevice: Refactor server greetings to separate method

--- src/rocketpower/library/SocketDevice.java
+++ src/rocketpower/library/SocketDevice.java
@@ -23,29 +23,49 @@ class SocketDevice extends RocketDevice {
     public SocketDevice(Logger logger, TrackContainer tracks, String host, int port) throws Exception {
         super(logger, tracks);
 
+        logger.fine(String.format("Trying to connect to Rocket at %s:%d", host, port));
+
         // Connect to rocket and initialize streams
         try {
             socket = new Socket(host, port);
         } catch (Exception e) {
-            logger.warning(String.format("Connection to %s:%d failed", host, port));
+            logger.warning(String.format("Connection to %s:%d failed.", host, port));
             throw e;
         }
         out = new DataOutputStream(socket.getOutputStream());
         in = new DataInputStream(socket.getInputStream());
 
+        logger.finer("Connection established.");
+        
+        // This raises if something goes terribly wrong
+        greetServer();
+
+        logger.info(String.format("Connected to Rocket running at %s:%d.", host, port));
+    }
+
+    private void greetServer() throws Exception {
+        logger.finer("Sending greetings to Rocket.");
+        
         // Write our greetings
         out.writeBytes(CLIENT_GREET);
 
+        logger.finer("Greetings sent. Now reading greetings from Rocket...");
+
         // Expect server to return with correct greetings
         byte greet[] = new byte[SERVER_GREET.length()];
-        in.readFully(greet, 0, SERVER_GREET.length());
-
-        if (!SERVER_GREET.equals(new String(greet))) {
-            logger.warning("Server didn't send correct greetings.");
-            throw new Exception("Greetings mismatch");
+        try {
+            in.readFully(greet, 0, SERVER_GREET.length());
+        } catch (Exception e) {
+            logger.severe("Couldn't read greetings from server.");
+            throw new Exception("Reading greetings failed");
         }
 
-        logger.info(String.format("Connected to server running at %s:%d", host, port));
+        logger.finer("Greetings read successfully.");
+
+        if (!SERVER_GREET.equals(new String(greet))) {
+            logger.severe("Server didn't send correct greetings.");
+            throw new Exception("Greetings mismatch");
+        }
     }
 
     /*
