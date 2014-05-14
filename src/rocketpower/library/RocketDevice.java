cac7c64ef9323d96ee4874252af40082fcc21578
Antti Hirvonen <antti.hirvonen@gmail.com>
2014-05-14 12:27:42 +0300

    RocketDevice: listen to TrackContainer changes
    Also added close-method to remove event listener.

--- src/rocketpower/library/RocketDevice.java
+++ src/rocketpower/library/RocketDevice.java
@@ -6,14 +6,26 @@ import java.util.logging.Logger;
 /*
  * Connection device baseclass
  */
-abstract class RocketDevice {
+abstract class RocketDevice implements TrackContainerListener {
     private TrackContainer tracks;
     protected Logger logger;
 
     RocketDevice(Logger logger, TrackContainer tracks) {
         this.logger = logger;
         this.tracks = tracks;
+        this.tracks.addEventListener(this);
+    }
+
+    /**
+     * If device has been initialized, it must be closed.
+     *
+     * Removes event listener.
+     */
+    public void close() {
+        this.tracks.removeEventListener(this);
     }
 
     abstract public void update();
+    public void trackAdded(String name) {}
+    public void trackDeleted(String name) {}
 }
