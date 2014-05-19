902ef096a83ad80394d345727ffeb4b5bcd48402
Antti Hirvonen <antti.hirvonen@gmail.com>
2014-05-19 15:34:16 +0300

    RocketController: observer pattern and keep track of fractional row
    Other objects can listen to events from controller, eg. react to row
    changes.

--- src/rocketpower/library/RocketController.java
+++ src/rocketpower/library/RocketController.java
@@ -1,6 +1,15 @@
 package rocketpower.library;
 
 
+import java.util.ArrayList;
+
+
+interface ControllerListener {
+    public void controllerStatusChanged(boolean isPlaying);
+    public void controllerRowChanged(int row);
+}
+
+
 /**
  * RocketController and it's subclasses
  * interface Rocket controllation with music
@@ -8,11 +17,38 @@ package rocketpower.library;
  */ 
 abstract class RocketController {
     /*
-     * For reading / settings current row,
-     * subclasses implement as necessary
+     * Keeping track of fractional rows in baseclass
+     * removes to need track time separately in subclasses
      */
-    abstract public int getCurrentRow();
-    abstract public void setCurrentRow(int row);
+    protected double currentRow;
+    ArrayList<ControllerListener> listeners;
+
+    public RocketController() {
+        listeners = new ArrayList<ControllerListener>();
+        currentRow = 0;
+    }
+
+    public void addEventListener(ControllerListener listener) {
+        listeners.add(listener);
+    }
+
+    public double getCurrentRow() {
+        return currentRow;
+    }
+
+    /**
+     * @param row new row value
+     * @param silent to suppress notifications
+     */
+    public void setCurrentRow(double row, boolean silent) {
+        // If whole row changes, notify listeners
+        if ((int)row != (int)currentRow && !silent) {
+            for (ControllerListener listener: listeners)
+                listener.controllerRowChanged((int)row);
+        }
+
+        currentRow = row;
+    }
 
     abstract public void update();
 
