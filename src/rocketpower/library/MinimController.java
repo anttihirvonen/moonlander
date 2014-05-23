bd95bc63af637b339abf7bcfaab265a8d4d2d6f4
Antti Hirvonen <antti.hirvonen@gmail.com>
2014-05-24 00:07:48 +0300

    Add MinimController and add minim.jar to build process

---
+++ src/rocketpower/library/MinimController.java
@@ -0,0 +1,30 @@
+package rocketpower.library;
+
+import ddf.minim.*;
+
+
+public class MinimController extends RocketController {
+    AudioPlayer player;
+    double rowsPerSecond;
+
+    public MinimController(AudioPlayer player, int beatsPerMinute, int rowsPerBeat) {
+        this.player = player;
+        rowsPerSecond = (beatsPerMinute / 60.0) * rowsPerBeat;
+    }
+
+    public void update() {
+        if (isPlaying())
+            setCurrentRow((player.position() / 1000.0) * rowsPerSecond, false);
+    }
+
+    public void pause() {
+        super.pause();
+        player.pause();
+    }
+
+    public void play() {
+        super.play();
+        player.cue((int)(getCurrentRow() * 1000.0 / rowsPerSecond));
+        player.play();
+    }
+}
